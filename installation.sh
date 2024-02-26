1bf26cfdbe4d0551ead413f766e6e1776d19aa1e
aliaviles <69882442+aliaviles@users.noreply.github.com>
2024-02-25 17:34:50 -0800

    Add files via upload

---
+++ installation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install Apache
+sudo apt install apache2 -y
+
+# Install MySQL
+sudo apt install mysql-server -y
+
+# Install PHP
+sudo apt install php libapache2-mod-php php-mysql -y
+
+# Install OpenSSH Server
+sudo apt install openssh-server -y
+
+# Install IPTables Firewall
+sudo apt install iptables-persistent
+
+# Install Snort Intrusion Detection System
+sudo apt install snort -y
+
+# Start services
+sudo systemctl start apache2
+sudo systemctl start mysql
+sudo systemctl start ssh
+sudo systemctl start snort
+
+# Enable services to start on boot
+sudo systemctl enable apache2
+sudo systemctl enable mysql
+sudo systemctl enable ssh
+sudo systemctl enable snort
